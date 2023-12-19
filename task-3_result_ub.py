2884ce2cc366aaeeb7e263ccd36dd4f64b5dcfea
abee1one <abee1@yandex.ru>
2023-12-19 23:44:25 +0300

    - made on_click, on_scroll event handlers;

--- task-3_result_ub.py
+++ task-3_result_ub.py
@@ -11,6 +11,100 @@ import clickhouse_connect
 import pandas as pd
 import pandahouse as ph
 
+
+def create_data_frame():
+    df = pd.DataFrame(columns=["x", "y", "deltaX", "deltaY", "clientTimeStamp", "button", "target"])
+    df[["x", "y", "deltaX", "deltaY"]] = df[["x", "y", "deltaX", "deltaY"]].astype(int)
+    df["clientTimeStamp"] = df["clientTimeStamp"].astype(float)
+    return df
+
+
+def get_current_window_title():
+    ewmh = EWMH()
+    # Get the currently active window
+    active_window = ewmh.getActiveWindow()
+    if active_window:
+        # Get the window title
+        window_title = ewmh.getWmName(active_window)
+        ewmh.display.flush()  # Ensure changes are processed
+        return window_title.decode('utf-8') if window_title else None
+    else:
+        return None
+
+
+def flush_buff(buff, buff_size):
+    if buff.shape[0] >= buff_size:
+        ph.to_clickhouse(buff, "mouse_movements", connection=pdh_conn, index=False)
+        buff = create_data_frame()
+        print(f"{buff_size} rows wrote in table")
+    return buff
+
+
+def on_move(x, y):
+    global currX
+    global currY
+    global df_buff
+    if currX == -1:
+        currX = x
+    if currY == -1:
+        currY = y
+    deltaX = abs(currX - x)
+    deltaY = abs(currY - y)
+    currX = x
+    currY = y
+    curr_datetime = datetime.now().timestamp()
+    button_name = "None"
+    window_title = get_current_window_title()
+    row = [x, y, deltaX, deltaY, curr_datetime, button_name, window_title]
+    df_buff.loc[len(df_buff.index)] = row
+    df_buff = flush_buff(df_buff, df_buff_size)
+
+
+def on_click(x, y, button, pressed):
+    global currX
+    global currY
+    global df_buff
+    if currX == -1:
+        currX = x
+    if currY == -1:
+        currY = y
+    deltaX = abs(currX - x)
+    deltaY = abs(currY - y)
+    currX = x
+    currY = y
+    curr_datetime = datetime.now().timestamp()
+    button_name = button
+    window_title = get_current_window_title()
+    row = [x, y, deltaX, deltaY, curr_datetime, button_name, window_title]
+    df_buff.loc[len(df_buff.index)] = row
+    df_buff = flush_buff(df_buff, df_buff_size)
+
+
+def on_scroll(x, y, dx, dy):
+    global df_buff
+    deltaX = abs(dx)
+    deltaY = abs(dy)
+    curr_datetime = datetime.now().timestamp()
+    button_name = "None"
+    window_title = get_current_window_title()
+    row = [x, y, deltaX, deltaY, curr_datetime, button_name, window_title]
+    df_buff.loc[len(df_buff.index)] = row
+    df_buff = flush_buff(df_buff, df_buff_size)
+
+
+def on_key_press(key):
+    print("!!! KEY PRESS !!!")
+
+
+def on_key_release(key):
+    if key == keyboard.Key.esc:
+        # Stop listeners
+        mouse_listener.stop()
+        keyboard_listener.stop()
+        print("Listeners stopped!")
+        return False
+
+
 df_buff = pd.DataFrame()
 df_buff_size = 50
 mouse_listener = None
@@ -25,13 +119,13 @@ cli_conn = clickhouse_connect.get_client(
     database="test",
     username="default",
     password="qw123456"
-    )
+)
 pdh_conn = {
     'host': 'http://127.0.0.1:8123',
     'database': 'test',
     'user': 'default',
     'password': 'qw123456'
-    }
+}
 print("CurrDB:", cli_conn.database.title())
 
 # Drop table
@@ -56,55 +150,6 @@ query_ddl = " \
           Order By clientTimeStamp"
 cli_conn.query(query_ddl)
 
-def CreateDataFrame():
-    df = pd.DataFrame(columns=["x", "y", "deltaX", "deltaY", "clientTimeStamp", "button", "target"])
-    df[["x", "y", "deltaX", "deltaY"]] = df[["x", "y", "deltaX", "deltaY"]].astype(int)
-    df["clientTimeStamp"] = df["clientTimeStamp"].astype(float)
-    return df
-
-def get_current_window_title():
-    ewmh = EWMH()
-    # Get the currently active window
-    active_window = ewmh.getActiveWindow()
-    if active_window:
-        # Get the window title
-        window_title = ewmh.getWmName(active_window)
-        ewmh.display.flush()  # Ensure changes are processed
-        return window_title.decode('utf-8') if window_title else None
-    else:
-        return None
-
-def on_move(x, y):
-    global currX
-    global currY
-    global df_buff
-    if currX == -1:
-        currX = x
-    if currY == -1:
-        currY = y
-    deltaX = abs(currX - x)
-    deltaY = abs(currY - y)
-    currX = x
-    currY = y
-    curr_datetime = datetime.now().timestamp()
-    sButton = "None"
-    window_title = get_current_window_title()
-    row = [x, y, deltaX, deltaY, curr_datetime, sButton, window_title]
-    df_buff.loc[len(df_buff.index)] = row
-    if df_buff.shape[0] > df_buff_size:
-        ph.to_clickhouse(df_buff, "mouse_movements", connection=pdh_conn, index=False)
-        df_buff = CreateDataFrame()
-    print(f'Mouse moved to ({x}, {y}) \
-          datetime: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")} \
-          win_title: {window_title}')
-
-
-#row = [100, 100, 10, 10, datetime.now().timestamp(), "None", "WinTitle01"]
-#df.loc[len(df.index)] = row
-#ph.to_clickhouse(df, "mouse_movements", connection=pdh_conn, index=False)
-
-
-
 
 """ 
 -- Create a table with Buffer engine
@@ -115,56 +160,10 @@ CREATE TABLE example_buffer_engine
     value Float64
 )
 ENGINE = Buffer('example_remote_table', 'example_distribution_key', 16, 10000);    
-    
 """
 
 
-query_ddl = '''
-create table test(a UInt8, b UInt8, c String) Engine MergeTree ORDER BY a;
-'''
-
-query_insert = '''
-insert into test(a,b,c) VALUES (1,2,'user_1') (1, 2, 'user_3') (1, 2, 'user_5')  (1, 3, 'user_1')  (1, 3, 'user_5') 
-'''
-
-query_select = '''
-select * from test where c = 'user_5'
-'''
-
-
-
-
-def on_click(x, y, button, pressed):
-    curr_datetime = datetime.now()
-    window_title = get_current_window_title()
-    action = 'pressed' if pressed else 'released'
-    print(f'Mouse {action} at ({x}, {y}) with {button} \
-          datetime: {curr_datetime.strftime("%Y-%m-%d %H:%M:%S")} \
-          win_title: {window_title}')
-
-
-def on_scroll(x, y, dx, dy):
-    curr_datetime = datetime.now()
-    window_title = get_current_window_title()
-    print(f'Scrolled at ({x}, {y}) with delta ({dx}, {dy}) \
-          datetime: {curr_datetime.strftime("%Y-%m-%d %H:%M:%S")} \
-          win_title: {window_title}')
-
-
-def on_key_press(key):
-    print("!!! KEY PRESS !!!")
-
-
-def on_key_release(key):
-    if key == keyboard.Key.esc:
-        # Stop listeners
-        mouse_listener.stop()
-        keyboard_listener.stop()
-        print("Listeners stopped!")
-        return False
-
-
-df_buff = CreateDataFrame()
+df_buff = create_data_frame()
 # Create listener instances
 mouse_listener = mouse.Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll)
 keyboard_listener = keyboard.Listener(on_release=on_key_release, on_press=on_key_press)
